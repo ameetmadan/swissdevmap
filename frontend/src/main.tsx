c65ddd58513f3ace75d2b04a0a5ced55d062f66a
Ameet Madan <ameetmadan@gmail.com>
2026-03-11 13:39:45 +0100

    fix: sentry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import axios from 'axios';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
 
 axios.defaults.baseURL = process.env.VITE_API_URL;
 
